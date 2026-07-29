ca826a2d609c6715982066e64b78c77559aa8261
Shepherd-bit <titusaoluoch@gmail.com>
2026-07-29 05:40:57 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/01-react-investor-landing-page/',
+  base: '/01-react-investor/',
 })
